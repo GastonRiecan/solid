950d1b71d19b4f28ff39e28bd1d6b517143e6682
Gaston Riecan <g.e.riecan@gmail.com>
2026-01-28 15:02:49 -0300

    Refactor SuperHero class for OCP compliance
    Refactor SuperHero class to follow Open/Closed Principle by introducing an abstract class and specific hero implementations.

--- python/open_closed.py
+++ python/open_closed.py
@@ -20,7 +20,43 @@ BETTER
 """
 # class SuperHero:
 # create a new version without breaking OCP
+class SuperHero:
+    def __init__(self, name):
+        self.name = name
 
+    def super_strength(self) -> str:
+        return f'{self.name} -> super strength'
+
+#SOLUTION:
+#Crear una clase abstracta para definir como debe estar compuesto un SuperHero
+class SuperHero(ABC):
+    def __init__(self, name):
+        self.name = name
+        
+    @abstractmethod
+    def activate_power(self) -> str:
+        pass
+
+#Crear una clase individual para cada SuperHero que herede de la clase abc
+class Superman(SuperHero):
+    def activate_power(self) -> str:
+        return f'{self.name} -> flying and super strength'
+
+class Hulk(SuperHero):
+    def activate_power(self) -> str:
+        return f'{self.name} -> super strength (SMASH!)'
+
+class InvisibleWoman(SuperHero):
+    def activate_power(self) -> str:
+        return f'{self.name} -> invisibility'
+
+# Instanciamos los héroes reales
+superman = Superman('Superman')
+hulk = Hulk('Hulk')
+invisible_woman = InvisibleWoman('Invisible Woman')
+
+
+        
 
 
 # running process
